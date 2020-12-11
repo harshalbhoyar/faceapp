43c717a5774febf708a35283f07cca9579f86848
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2020-12-11 14:18:36 +0530

    Post model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
+	has_many :posts
 	mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         :omniauthable, omniauth_providers: %i[facebook]
+ #        :omniauthable, omniauth_providers: %i[facebook]
    validate :picture_size
 
    private
-   
+
    def picture_size 
    	errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
    end
