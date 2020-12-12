911f8b074887f50a96b91c67c71979734d8a6b85
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2020-12-12 15:22:20 +0530

    Added frienship controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
 	has_many :likes, dependent: :destroy
+	has_many :friend_sent, class_name: 'Friendship',
+	                        foreign_key: 'sent_by_id',
+	                        inverse_of: 'sent_by',	
+	                        dependent: :destroy
+	has_many :friend_request, class_name: 'Friendship',
+	                        foreign_key: 'sent_to_id',
+	                        inverse_of: 'sent_to_id',
+	                        dependent: :destroy
+	has_many :friends, ->{merge(Friendship.friends)},
+	          through: :friend_sent, source: :sent_to
+	has_many :pending_requests, -> { merge(Friendship.not_friends) },
+	          through: :friend_sent, source: :sent_to
+	has_many :received_requests, -> { merge(Friendship.not_friends) },
+	          through: :friend_requests, source: :sent_by          
+	                                                                    
 	mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_153636) do
+ActiveRecord::Schema.define(version: 2020_12_12_074347) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "sent_to_id", null: false
+    t.bigint "sent_by_id", null: false
+    t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sent_by_id"], name: "index_friendships_on_sent_by_id"
+    t.index ["sent_to_id"], name: "index_friendships_on_sent_to_id"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id"
+    t.bigint "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_likes_on_comment_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +66,12 @@ ActiveRecord::Schema.define(version: 2020_12_10_153636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "friendships", "users", column: "sent_by_id"
+  add_foreign_key "friendships", "users", column: "sent_to_id"
+  add_foreign_key "likes", "comments"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
